d9e76843292bda689fb7fcdc455a9567a8755890
Sir Lancealot <Victortruffin10@yahoo.com>
2020-06-25 21:50:28 -0500

    TicTacToe

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,30 @@
+#include "Tic_tac_toe.h"
+
+
+
 
 int main() 
 {
+	TicTacToe tic_tac_toe;
+	std::string player;
+
+	std::cout<<"Enter X or 0: ";
+	std::cin>>player;
+
+	tic_tac_toe.start_game(player);
+
+	do
+	{
+		int position;
+		std::cout<<"Enter position from 1 to 9: ";
+		std::cin>>position;
+		tic_tac_toe.mark_board(position);
+		tic_tac_toe.display_board();
+	} while (tic_tac_toe.game_over()==false);
+
+	std::cout<<"GAME OVER";
+	
+
 
 	return 0;
 }
